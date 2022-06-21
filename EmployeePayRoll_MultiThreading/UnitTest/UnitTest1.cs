c9072a7e4ffb6bc965ab456765f09be7c0279498
Shashank <langishashank@gmail.com>
2022-06-21 19:51:20 +0530

    [Shashank]:Added Employee Details.

--- EmployeePayRoll_MultiThreading/UnitTest/UnitTest1.cs
+++ EmployeePayRoll_MultiThreading/UnitTest/UnitTest1.cs
@@ -19,11 +19,11 @@ namespace EmployeePayRollTestCases
         [Test]
         public void CheckingTime_Required_For_Inserting_Details()
         {
-            employeeDetails.Add(new EmployeeDetails(ID: 1, Name: "John", StartDate: Convert.ToDateTime("2024-05-12"), Gender: "M", PhoneNumber: 8546321452, Address: "Miami", Department: "Claims", BasicPay: 62000, Deduction: 300, TaxablePay: 200, IncomeTax: 100, NetPay: 61400));
-            employeeDetails.Add(new EmployeeDetails(ID: 43, Name: "Charles", StartDate: Convert.ToDateTime("2024-07-17"), Gender: "M", PhoneNumber: 8475123698, Address: "Miami", Department: "Claims", BasicPay: 62000, Deduction: 300, TaxablePay: 200, IncomeTax: 100, NetPay: 61400));
-            employeeDetails.Add(new EmployeeDetails(ID: 43, Name: "Hatie", StartDate: Convert.ToDateTime("2024-06-24"), Gender: "F", PhoneNumber: 8741245369, Address: "Miami", Department: "Claims", BasicPay: 62000, Deduction: 300, TaxablePay: 200, IncomeTax: 100, NetPay: 61400));
-            employeeDetails.Add(new EmployeeDetails(ID: 43, Name: "Pablo", StartDate: Convert.ToDateTime("2024-01-08"), Gender: "M", PhoneNumber: 8563269874, Address: "Miami", Department: "Claims", BasicPay: 62000, Deduction: 300, TaxablePay: 200, IncomeTax: 100, NetPay: 61400));
-            employeeDetails.Add(new EmployeeDetails(ID: 43, Name: "Serena", StartDate: Convert.ToDateTime("2024-03-02"), Gender: "F", PhoneNumber: 745124639, Address: "Miami", Department: "Claims", BasicPay: 62000, Deduction: 300, TaxablePay: 200, IncomeTax: 100, NetPay: 61400));
+            employeeDetails.Add(new EmployeeDetails(ID: 5, Name: "John", StartDate: Convert.ToDateTime("2024-05-12"), Gender: "M", PhoneNumber: 8546321452, Address: "Miami", Department: "Claims", BasicPay: 62000, Deduction: 300, TaxablePay: 200, IncomeTax: 100, NetPay: 61400));
+            employeeDetails.Add(new EmployeeDetails(ID: 24, Name: "Charles", StartDate: Convert.ToDateTime("2024-07-17"), Gender: "M", PhoneNumber: 8475123698, Address: "Miami", Department: "Claims", BasicPay: 62000, Deduction: 300, TaxablePay: 200, IncomeTax: 100, NetPay: 61400));
+            employeeDetails.Add(new EmployeeDetails(ID: 24, Name: "Hatie", StartDate: Convert.ToDateTime("2024-06-24"), Gender: "F", PhoneNumber: 8741245369, Address: "Miami", Department: "Claims", BasicPay: 62000, Deduction: 300, TaxablePay: 200, IncomeTax: 100, NetPay: 61400));
+            employeeDetails.Add(new EmployeeDetails(ID: 27, Name: "Pablo", StartDate: Convert.ToDateTime("2024-01-08"), Gender: "M", PhoneNumber: 8563269874, Address: "Miami", Department: "Claims", BasicPay: 62000, Deduction: 300, TaxablePay: 200, IncomeTax: 100, NetPay: 61400));
+            employeeDetails.Add(new EmployeeDetails(ID: 27, Name: "Serena", StartDate: Convert.ToDateTime("2024-03-02"), Gender: "F", PhoneNumber: 745124639, Address: "Miami", Department: "Claims", BasicPay: 62000, Deduction: 300, TaxablePay: 200, IncomeTax: 100, NetPay: 61400));
 
             DateTime StartDateTime = DateTime.Now;
             emppayroll.AddEmployee(employeeDetails);
